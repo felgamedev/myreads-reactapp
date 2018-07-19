9e5c14d5111f1959dc495c5195798e9dc5bddc7e
felgamedev <felgamedev@gmail.com>
2018-07-19 14:26:03 -0300

    Initial set up for new implementation
    Uses the BooksAPI to call to the server, logging the result for 
    debugging

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -14,8 +14,14 @@ class BooksApp extends React.Component {
   }
 
   render() {
+    // Test that the API is working and preview currently saved books
+    BooksAPI.getAll().then(response => console.log(response))
+
     return (
       <div className="app">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
       </div>
     )
   }
