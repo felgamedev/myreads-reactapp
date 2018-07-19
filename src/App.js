36a7f51db4c1e63749d8178032bddff5cc0a4c2f
felgamedev <felgamedev@gmail.com>
2018-07-19 14:45:09 -0300

    Get all books using API and save to state

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Bookshelf from './components/Bookshelf'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    bookshelves: [ "read", "wantToRead", "currentlyReading"],
+    allBooks: {}
+  }
+
+  componentWillMount(){
+    BooksAPI.getAll()
+    .then(allBooks => {
+      this.setState({allBooks})
+    })
   }
 
   render() {
-    // Test that the API is working and preview currently saved books
-    BooksAPI.getAll().then(response => console.log(response))
 
     return (
       <div className="app">
